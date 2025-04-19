937bd86e6d217bc6e22f2679adea3e9819c2a3f0
CristopherGamboa <cristophergamboa@outlook.com>
2025-04-19 00:44:26 -0400

    Implement CRUD to services

--- src/main/java/com/petcare/petcare/services/ServiceService.java
+++ src/main/java/com/petcare/petcare/services/ServiceService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.petcare.petcare.dtos.ServiceRequest;
 import com.petcare.petcare.models.Service;
 import com.petcare.petcare.repositories.interfaces.IServiceRepository;
 import com.petcare.petcare.services.interfaces.IServiceService;
@@ -29,5 +30,36 @@ public class ServiceService implements IServiceService {
         return serviceRepository.findById(id);
     }
 
-    
+    @Override
+    public Optional<Service> save(ServiceRequest request) {
+        Service service = Service.builder()
+            .name(request.getName())
+            .price(request.getPrice())
+            .description(request.getDescription())
+            .build();
+
+        return Optional.of(serviceRepository.save(service));
+    }
+
+    @Override
+    public Optional<Service> update(Long id, ServiceRequest request) {
+        if (!serviceRepository.existsById(id)) {
+            return Optional.empty();
+        }
+
+        Service service = Service.builder()
+            .id(id)
+            .name(request.getName())
+            .price(request.getPrice())
+            .description(request.getDescription())
+            .build();
+
+
+        return Optional.of(serviceRepository.save(service));
+    }
+
+    @Override
+    public void delete(Long id) {
+        serviceRepository.deleteById(id);
+    } 
 }
