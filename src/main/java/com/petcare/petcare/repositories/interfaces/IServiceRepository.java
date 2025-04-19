a9e938a70decfb6d0dd2f93a61439c8e2762782f
CristopherGamboa <cristophergamboa@outlook.com>
2025-04-19 00:45:05 -0400

    Add JPA repositories

--- src/main/java/com/petcare/petcare/repositories/interfaces/IServiceRepository.java
+++ src/main/java/com/petcare/petcare/repositories/interfaces/IServiceRepository.java
@@ -1,11 +1,7 @@
 package com.petcare.petcare.repositories.interfaces;
 
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.petcare.petcare.models.Service;
 
-public interface IServiceRepository {
-    public Optional<Service> findById(Long id); 
-    public List<Service> findAll();
-}
+public interface IServiceRepository extends JpaRepository<Service, Long> { }
