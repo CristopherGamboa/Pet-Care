e8b5354266ac4f569a87e8d1816d32636b89360c
CristopherGamboa <cristophergamboa@outlook.com>
2025-04-19 01:38:10 -0400

    Add Error Response DTO

---
+++ src/main/java/com/petcare/petcare/dtos/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.petcare.petcare.dtos;
+
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+    private String message;
+    private String details;
+    private Map<String, String> validationErrors;
+}
